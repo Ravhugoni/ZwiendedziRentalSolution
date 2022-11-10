0b389aaa40beb4067d775f4f7cb9f853743afc3c
Cheyeza <94190190+Cheyeza@users.noreply.github.com>
2022-11-10 16:11:13 +0200

    pulling

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CarsService } from 'src/app/services/cars.service';
+
+export interface car{
+  
+  carName:string;
+  carImage:string;
+  model:string;
+  numberPlate:string;
+  make:string;
+  price:string;
+  companyID:string;
+  id:string;
+}
 
 @Component({
   selector: 'app-header',
@@ -6,10 +19,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  carDetails : car[] =  [{carName:"",carImage:"",model:"",numberPlate:"",make:"",price:"",companyID:"",id:""}]
 
-  constructor() { }
+  constructor(private carServ:CarsService) { }
 
   ngOnInit(): void {
+
+    this.carServ.GetCarsByCategory(this.carDetails).subscribe((result)=>{
+      if(result.error) throw result.message
+      else
+      this.carDetails = result.data
+    })
   }
 
 }
