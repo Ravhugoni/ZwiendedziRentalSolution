8b7ff2da6351a06096edc87d206b8f43b053f90b
Cheyeza <94190190+Cheyeza@users.noreply.github.com>
2022-11-30 11:12:29 +0200

    backend

---
+++ backend/Controllers/numUsers.js
@@ -0,0 +1,29 @@
+const Pool = require('pg').Pool
+const pool = require("../connection")
+
+
+  const getAllUsers = (req,res)=>{
+
+    pool.query('SELECT COUNT(users.id) AS numUsers FROM public.users;',(error ,results)=>{
+    if(error){
+        throw error
+    }
+    res.status(200).json(results.rows)
+    })
+  }
+
+  const getUsersBYReg = (req,res)=>{
+
+    pool.query("SELECT count(id) as numuser, TO_CHAR(created_at, 'YYYY-mm') AS created_at FROM public.users GROUP BY TO_CHAR(created_at, 'YYYY-mm') ORDER BY TO_CHAR(created_at, 'YYYY-mm') ASC;",(error ,results)=>{
+    if(error){
+        throw error
+    }
+    res.status(200).json(results.rows)
+    })
+  }
+
+ 
+  module.exports = {
+    getAllUsers,
+    getUsersBYReg
+  }
